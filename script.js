cce9ae22929eaad4c48acc1bc02e0a24841a968a
Ruan Vinícius <contatoruanvinicius@gmail.com>
2022-05-10 17:25:23 -0300

    - Refactor

--- script.js
+++ script.js
@@ -1,346 +1,283 @@
 Array.prototype.random = function () {
-  const result = Math.random() * this.length;
-  return this[Math.floor(result)];
+  return this[Math.floor(Math.random() * this.length)];
 };
 
-const ambiente = [
-  [[], [], [], [], []],
-  [[], [], [], [], []],
-  [[], [], [], [], []],
-  [[], [], [], [], []],
-  [[], [], [], [], []],
-];
-
-const formiga = { valor: "F", quantidade: 0, limite: 1 };
-const comida = { valor: "C", quantidade: 0, limite: 7, min: 1 };
-const ninho = { valor: "N", quantidade: 0, limite: 1 };
-const predador = { valor: "P", quantidade: 0, limite: 6 };
-const vazio = { valor: "V", quantidade: 0, limite: -1 };
-
-const movimentos = ["começo"];
-let comidasArmazenadas = 0;
-
-let formigaCoordenadas;
-let comidasCoordenadas = [];
-let ninhoCoordenadas = {};
-let unidadeFormiga = "F";
-
-const gerarAmbiente = () => {
-  ambiente.forEach((linha, indexLinha) => {
-    linha.forEach((celula, indexCelula) => {
-      celula = elementoAleatorio();
-      ambiente[indexLinha][indexCelula] = celula?.valor;
-    });
-  });
-
-  mostrarAmbiente();
-  return ambiente;
-};
-
-const elementoAleatorio = () => {
-  const elementos = [comida, ninho, predador, formiga];
-  const elementoAtual = elementos.random();
-
-  if (elementoAtual.quantidade !== elementoAtual.limite) {
-    elementoAtual.quantidade++;
-    return elementoAtual;
+class Formiga {
+  valor = "F";
+  coluna;
+  linha;
+  ambiente;
+  movimentos = ["nascer"];
+  coordenadas;
+  comidasCapturadas = 0;
+  cont = 0;
+  chegouNoNinho = false;
+  constructor(linha, coluna, ambiente = new Ambiente()) {
+    this.linha = linha;
+    this.coluna = coluna;
+    this.ambiente = ambiente;
+    this.coordenadas = { linha: this.linha, coluna: this.coluna };
   }
 
-  return vazio;
-};
-
-function estaVazio(linha, coluna) {
-  return ambiente[linha][coluna].includes("V");
-}
-
-function temComida(linha, coluna) {
-  return ambiente[linha][coluna].includes("C");
-}
-
-function temNinho(linha, coluna) {
-  return ambiente[linha][coluna].includes("N");
-}
-
-function temPredador(linha, coluna) {
-  return ambiente[linha][coluna].includes("P");
-}
-
-const mostrarAmbiente = () => {
-  console.table(ambiente);
-};
-
-gerarAmbiente();
-
-const altura = ambiente.length - 1;
-
-/**
- * Se eu estiver na primeira linha não existe linha acima.
- * @param {*} linha
- * @returns boolean
- */
-function temLinhaAcima(linha) {
-  return linha > 0;
-}
-/**
- * Se eu estiver na última linha não posso descer.
- * @param {*} linha
- * @returns
- */
-function temLinhaAbaixo(linha) {
-  return linha < altura;
-}
-
-function temColunaDireita(linha, coluna) {
-  return !!ambiente[linha][coluna + 1];
-}
-function temColunaEsquerda(linha, coluna) {
-  return !!ambiente[linha][coluna - 1];
-}
-
-function podeIrCima(linha, coluna) {
-  return temLinhaAcima(linha) && !temPredador(linha - 1, coluna);
-}
-
-function podeIrBaixo(linha, coluna) {
-  return temLinhaAbaixo(linha) && !temPredador(linha + 1, coluna);
-}
-
-function podeIrDireita(linha, coluna) {
-  return temColunaDireita(linha, coluna) && !temPredador(linha, coluna + 1);
-}
-
-function podeIrEsquerda(linha, coluna) {
-  return temColunaEsquerda(linha, coluna) && !temPredador(linha, coluna - 1);
-}
-
-function buscarNinho() {
-  ambiente.map((linha, indexLinha) =>
-    linha.map((coluna, indexColuna) => {
-      if (coluna.includes("N")) {
-        ninhoCoordenadas = { linha: indexLinha, coluna: indexColuna };
-      }
-    })
-  );
-
-  console.log({ ninhoCoordenadas });
-  return ninhoCoordenadas;
-}
-
-function buscarComidas() {
-  ambiente.map((linha, indexLinha) => {
-    if (linha.findIndex((valor) => valor.includes("C")) > -1) {
-      linha.map((coluna, indexColuna) => {
-        coluna.includes("C")
-          ? comidasCoordenadas.push({
-              linha: indexLinha,
-              coluna: indexColuna,
-              pega: false,
-            })
-          : {};
-      });
-    }
-  });
-
-  console.log({ comidasCoordenadas });
-  return comidasCoordenadas;
-}
-
-// verificar qual é o ponto de comida mais próximo
-function buscarComidaMaisProxima() {
-  console.log("Buscando nova rota de comida");
-
-  const comidas = comidasCoordenadas.filter((comida) => !comida.pega);
-  console.log("Quantidade de comidas a serem recolhidas: ", comidas.length);
-  if (comidas.length > 0) {
-    comidaMaisProxima = comidasCoordenadas
-      .filter((comida) => !comida.pega)
-      .reduce(function (anterior, corrente) {
-        return Math.abs(corrente.linha - formigaCoordenadas.linha) +
-          Math.abs(corrente.coluna - formigaCoordenadas.coluna) <=
-          Math.abs(anterior.linha - formigaCoordenadas.linha) +
-            Math.abs(anterior.coluna - formigaCoordenadas.coluna)
-          ? corrente
-          : anterior;
-      });
-
-    console.log("A comida mais próxima está em: ", comidaMaisProxima);
-    return irParaComidaMaisProxima();
-  } else {
-    alert("ACABOU AS COMIDAS");
-    return false;
-  }
-}
-
-function buscarFormiga() {
-  ambiente.map((linha, indexLinha) => {
-    linha.map((coluna, indexColuna) => {
-      if (coluna === "F") {
-        console.log({ indexLinha }, { indexColuna });
-        formigaCoordenadas = { linha: indexLinha, coluna: indexColuna };
-        return coluna;
-      }
-    });
-  });
-  console.log({ formigaCoordenadas });
-}
-
-function atualizarCelulaAnterior(linhaAtual, colunaAtual) {
-  if (ambiente[linhaAtual][colunaAtual]) {
-    ambiente[linhaAtual][colunaAtual] = temNinho(linhaAtual, colunaAtual)
-      ? "N"
+  modificarAmbienteAnterior() {
+    this.ambiente.ambiente[this.linha][this.coluna] = this.ambiente.temNinho(
+      this.linha,
+      this.coluna
+    )
+      ? `N`
       : "V";
   }
-  return ambiente;
-}
 
-function atualizarColunaAtual(linha, coluna) {
-  if (ambiente[linha][coluna]) {
-    ambiente[linha][coluna] = estaVazio(linha, coluna)
-      ? `${unidadeFormiga}`
-      : `${ambiente[linha][coluna]}${unidadeFormiga}`;
-
-    setTimeout(() => {
-      temComida(linha, coluna) ? pegarComida() : {};
-    }, 2000);
-  }
-
-  return ambiente;
-}
-
-function pegarComida() {
-  console.log("Formiga pegou uma comida");
-
-  comidasCoordenadas.map((comida) => {
-    if (
-      comida.linha === formigaCoordenadas.linha &&
-      comida.coluna === formigaCoordenadas.coluna
-    ) {
-      comida.pega = true;
-      comidasArmazenadas++;
-      unidadeFormiga = `F${"C".repeat(comidasArmazenadas)}`;
-      return comida;
-    }
-    return comida;
-  });
-  console.log({ comidasArmazenadas });
-  setTimeout(() => {
-    buscarComidaMaisProxima();
-  }, 2000);
-  return ambiente;
-}
-
-function descer(linhaAtual, colunaAtual) {
-  console.log("formiga está descendo");
-  formigaCoordenadas.linha++;
-
-  atualizarCelulaAnterior(linhaAtual, colunaAtual);
-  atualizarColunaAtual(formigaCoordenadas.linha, colunaAtual);
-  // console.table(ambiente);
-  return ambiente;
-}
-
-function subir(linhaAtual, colunaAtual) {
-  console.log("formiga está subindo");
-  formigaCoordenadas.linha--;
-
-  atualizarCelulaAnterior(linhaAtual, colunaAtual);
-  atualizarColunaAtual(formigaCoordenadas.linha, colunaAtual);
-
-  // console.table(ambiente);
-  return ambiente;
-}
-
-function direita(linhaAtual, colunaAtual) {
-  console.log("formiga está indo para direita");
-  formigaCoordenadas.coluna++;
-  atualizarCelulaAnterior(linhaAtual, colunaAtual);
-  atualizarColunaAtual(linhaAtual, formigaCoordenadas.coluna);
-
-  // console.table(ambiente);
-  return ambiente;
-}
-
-function esquerda(linhaAtual, colunaAtual) {
-  console.log("formiga está indo para esquerda");
-  formigaCoordenadas.coluna--;
-  atualizarCelulaAnterior(linhaAtual, colunaAtual);
-  atualizarColunaAtual(linhaAtual, formigaCoordenadas.coluna);
-  // console.table(ambiente);
-  return ambiente;
-}
-
-function irParaComidaMaisProxima() {
-  console.log({ comidasArmazenadas }, comidasCoordenadas.length);
-  // while (comidasArmazenadas < 1) {
-  cont = 0;
-  while (comidasArmazenadas !== comidasCoordenadas.length) {
-    cont++;
-    console.table(ambiente);
-
-    if (cont > 199) {
-      console.log("fourcou o vreak");
-      break;
-    }
-    if (
-      formigaCoordenadas.coluna > comidaMaisProxima.coluna &&
-      podeIrEsquerda(formigaCoordenadas.linha, formigaCoordenadas.coluna)
-    ) {
-      movimentos.push("esquerda");
-      return esquerda(formigaCoordenadas.linha, formigaCoordenadas.coluna);
-    } else if (
-      formigaCoordenadas.coluna < comidaMaisProxima.coluna &&
-      podeIrDireita(formigaCoordenadas.linha, formigaCoordenadas.coluna)
-    ) {
-      movimentos.push("direita");
-      return direita(formigaCoordenadas.linha, formigaCoordenadas.coluna);
+  modificarAmbientePosterior() {
+    if (this.ambiente.temComida(this.linha, this.coluna)) {
+      this.ambiente.ambiente[this.linha][this.coluna] = this.pegarComida();
+    } else if (this.ambiente.temNinho(this.linha, this.coluna)) {
+      this.ambiente.ambiente[this.linha][this.coluna] = `N${this.valor}`;
     } else {
-      if (
-        formigaCoordenadas.linha < comidaMaisProxima.linha &&
-        podeIrBaixo(formigaCoordenadas.linha, formigaCoordenadas.coluna)
-      ) {
-        movimentos.push("descer");
-        return descer(formigaCoordenadas.linha, formigaCoordenadas.coluna);
-      } else if (
-        formigaCoordenadas.linha > comidaMaisProxima.linha &&
-        podeIrCima(formigaCoordenadas.linha, formigaCoordenadas.coluna)
-      ) {
-        movimentos.push("subir");
-        return subir(formigaCoordenadas.linha, formigaCoordenadas.coluna);
-      } else {
-        if (
-          !podeIrEsquerda(formigaCoordenadas.linha, formigaCoordenadas.coluna)
-        ) {
-          direita(formigaCoordenadas.linha, formigaCoordenadas.coluna);
-        } else if (
-          !podeIrDireita(formigaCoordenadas.linha, formigaCoordenadas.coluna)
-        ) {
-          esquerda(formigaCoordenadas.linha, formigaCoordenadas.coluna);
-        } else if (
-          !podeIrCima(formigaCoordenadas.linha, formigaCoordenadas.coluna)
-        ) {
-          descer(formigaCoordenadas.linha, formigaCoordenadas.coluna);
-        } else {
-          console.log("else 2");
-          subir(formigaCoordenadas.linha, formigaCoordenadas.coluna);
-        }
-        console.log("não pode ir pra nenhum lugar");
-        // irParaComidaMaisProxima();
-      }
-      break;
+      this.ambiente.ambiente[this.linha][this.coluna] = this.valor;
+    }
+    this.ambiente.mostrar();
+    const novaLinhaAlvo = this.ambiente.comidaMaisProximaLocalizacao(
+      this.linha,
+      this.coluna
+    )?.linha;
+    const novaColunaAlvo = this.ambiente.comidaMaisProximaLocalizacao(
+      this.linha,
+      this.coluna
+    )?.coluna;
+    if (this.ambiente.comidasRestantes == 0 && !this.chegouNoNinho) {
+      return this.irAteNinho(
+        this.ambiente.ninhoLocalizacao().linha,
+        this.ambiente.ninhoLocalizacao().coluna
+      );
+    } else if (this.ambiente.comidasRestantes > 0) {
+      return this.irAte(novaLinhaAlvo, novaColunaAlvo);
+    } else {
+      return false;
     }
   }
-  console.log({ movimentos });
-  console.table(ambiente);
+
+  pegarComida() {
+    this.comidasCapturadas++;
+    this.ambiente.comidasRestantes--;
+    const comida = this.ambiente.comidas.find(
+      (comida) => comida.coluna === this.coluna && comida.linha === this.linha
+    );
+    comida.pega = true;
+    this.valor = `F${"C".repeat(this.comidasCapturadas)}`;
+    return this.valor;
+  }
+
+  irDireita() {
+    this.modificarAmbienteAnterior();
+    this.coluna++;
+    return this.modificarAmbientePosterior();
+  }
+
+  irEsquerda() {
+    this.modificarAmbienteAnterior();
+    this.coluna--;
+    return this.modificarAmbientePosterior();
+  }
+
+  irCima() {
+    this.modificarAmbienteAnterior();
+    this.linha--;
+    return this.modificarAmbientePosterior();
+  }
+
+  irBaixo() {
+    this.modificarAmbienteAnterior();
+    this.linha++;
+    return this.modificarAmbientePosterior();
+  }
+
+  irAte(linhaAlvo, colunaAlvo) {
+    while (this.ambiente.comidasRestantes !== 0) {
+      this.cont++;
+
+      if (this.coluna > colunaAlvo) {
+        this.movimentos.push("esquerda");
+        return this.irEsquerda(this.linha, this.coluna);
+      } else if (this.coluna < colunaAlvo) {
+        this.movimentos.push("direita");
+        return this.irDireita();
+      } else {
+        console.log("coluna igual");
+        if (this.linha > linhaAlvo) {
+          this.movimentos.push("cima");
+          return this.irCima();
+        } else {
+          this.movimentos.push("baixo");
+          return this.irBaixo();
+        }
+      }
+    }
+    return;
+  }
+
+  irAteNinho(linhaAlvo, colunaAlvo) {
+    while (this.linha !== linhaAlvo || this.coluna !== colunaAlvo) {
+      if (this.coluna > colunaAlvo) {
+        this.movimentos.push("esquerda");
+        return this.irEsquerda(this.linha, this.coluna);
+      } else if (this.coluna < colunaAlvo) {
+        this.movimentos.push("direita");
+        return this.irDireita();
+      } else {
+        console.log("coluna igual");
+        if (this.linha > linhaAlvo) {
+          this.movimentos.push("cima");
+          return this.irCima();
+        } else {
+          this.movimentos.push("baixo");
+          return this.irBaixo();
+        }
+      }
+    }
+    this.chegouNoNinho = true;
+    console.log("chegou aq");
+    this.ambiente.mostrar();
+    return;
+  }
 }
 
-const jogar = () => {
-  buscarFormiga();
-  buscarComidas();
-  buscarNinho();
+class Ambiente {
+  ambiente;
+  comidas;
+  comidasRestantes = 0;
 
-  setTimeout(() => {
-    buscarComidaMaisProxima();
-  }, 2000);
-};
+  constructor() {
+    this.gerar();
+    this.comidas = this.mapearComidas();
+    this.comidasRestantes = this.comidas.length;
+  }
+  gerar() {
+    const ambiente = [
+      [[], [], [], [], []],
+      [[], [], [], [], []],
+      [[], [], [], [], []],
+      [[], [], [], [], []],
+      [[], [], [], [], []],
+    ];
 
-jogar();
+    let formiga = { valor: "F", quantidade: 0, limite: 1 };
+    let comida = { valor: "C", quantidade: 0, limite: 3, min: 1 };
+    let ninho = { valor: "N", quantidade: 0, limite: 1 };
+    let predador = { valor: "P", quantidade: 0, limite: 0 };
+    let vazio = { valor: "V", quantidade: 0, limite: -1 };
+    let elementos = [comida, ninho, predador, formiga];
+
+    ambiente.forEach((linha, indexLinha) => {
+      linha.forEach((celula, indexCelula) => {
+        let elementoAux = elementos.random();
+        let celulaValor =
+          elementoAux.quantidade !== elementoAux.limite ? elementoAux : vazio;
+
+        elementos.map((elemento) => {
+          if (elemento.valor === celulaValor.valor) {
+            elemento.quantidade++;
+            return elemento;
+          }
+        });
+        celula = celulaValor;
+        ambiente[indexLinha][indexCelula] = celula?.valor;
+      });
+    });
+    return (this.ambiente = ambiente);
+  }
+
+  estaVazio(linha, coluna) {
+    return this.ambiente[linha][coluna].includes("V");
+  }
+
+  temComida(linha, coluna) {
+    return this.ambiente[linha][coluna].includes("C");
+  }
+
+  temNinho(linha, coluna) {
+    return this.ambiente[linha][coluna].includes("N");
+  }
+
+  temPredador(linha, coluna) {
+    return this.ambiente[linha][coluna].includes("P");
+  }
+
+  formigaLocalizacao() {
+    let formigaCoordenadas;
+    this.ambiente.map((linha, indexLinha) => {
+      linha.map((coluna, indexColuna) => {
+        if (coluna === "F") {
+          formigaCoordenadas = { linha: indexLinha, coluna: indexColuna };
+          return coluna;
+        }
+      });
+    });
+
+    return formigaCoordenadas;
+  }
+
+  ninhoLocalizacao() {
+    let ninhoCoordenadas;
+    this.ambiente.map((linha, indexLinha) => {
+      linha.map((coluna, indexColuna) => {
+        if (coluna.includes("N")) {
+          ninhoCoordenadas = { linha: indexLinha, coluna: indexColuna };
+          return coluna;
+        }
+      });
+    });
+
+    return ninhoCoordenadas;
+  }
+
+  mapearComidas() {
+    let comidasCoordenadas = [];
+    this.ambiente.map((linha, indexLinha) => {
+      if (linha.findIndex((valor) => valor.includes("C")) > -1) {
+        linha.map((coluna, indexColuna) => {
+          coluna.includes("C")
+            ? comidasCoordenadas.push({
+                linha: indexLinha,
+                coluna: indexColuna,
+                pega: false,
+              })
+            : {};
+        });
+      }
+    });
+
+    return comidasCoordenadas;
+  }
+
+  comidaMaisProximaLocalizacao(linha, coluna) {
+    this.comidas = this.comidas.filter((comida) => !comida.pega);
+    if (this.comidas.length >= 1) {
+      let comidaMaisProxima = this.comidas
+        .filter((comida) => !comida.pega)
+        .reduce(function (anterior, corrente) {
+          return Math.abs(corrente.linha - linha) +
+            Math.abs(corrente.coluna - coluna) <=
+            Math.abs(anterior.linha - linha) +
+              Math.abs(anterior.coluna - coluna)
+            ? corrente
+            : anterior;
+        });
+      return comidaMaisProxima;
+    }
+  }
+
+  mostrar() {
+    console.table(this.ambiente);
+  }
+}
+
+ambiente = new Ambiente();
+
+formiga = new Formiga(
+  ambiente.formigaLocalizacao().linha,
+  ambiente.formigaLocalizacao().coluna,
+  ambiente
+);
+
+formiga.irAte();
