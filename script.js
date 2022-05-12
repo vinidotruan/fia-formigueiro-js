a17420c5f25300abaa3bb38c9b5b37be6421a6ad
Ruan Vinícius <contatoruanvinicius@gmail.com>
2022-05-11 21:11:51 -0300

    - Removendo toda complexidade e deixando apenas a reatividade do código

--- script.js
+++ script.js
@@ -16,126 +16,47 @@ class Formiga {
     this.linha = linha;
     this.coluna = coluna;
     this.ambiente = ambiente;
-    this.coordenadas = { linha: this.linha, coluna: this.coluna };
-  }
-
-  modificarAmbienteAnterior() {
-    this.ambiente.ambiente[this.linha][this.coluna] = this.ambiente.temNinho(
-      this.linha,
-      this.coluna
-    )
-      ? `N`
-      : "V";
-  }
-
-  modificarAmbientePosterior() {
-    if (this.ambiente.temComida(this.linha, this.coluna)) {
-      this.ambiente.ambiente[this.linha][this.coluna] = this.pegarComida();
-    } else if (this.ambiente.temNinho(this.linha, this.coluna)) {
-      this.ambiente.ambiente[this.linha][this.coluna] = `N${this.valor}`;
-    } else {
-      this.ambiente.ambiente[this.linha][this.coluna] = this.valor;
-    }
-    this.ambiente.mostrar();
-    const novaLinhaAlvo = this.ambiente.comidaMaisProximaLocalizacao(
-      this.linha,
-      this.coluna
-    )?.linha;
-    const novaColunaAlvo = this.ambiente.comidaMaisProximaLocalizacao(
-      this.linha,
-      this.coluna
-    )?.coluna;
-    if (this.ambiente.comidasRestantes == 0 && !this.chegouNoNinho) {
-      return this.irAteNinho(
-        this.ambiente.ninhoLocalizacao().linha,
-        this.ambiente.ninhoLocalizacao().coluna
-      );
-    } else if (this.ambiente.comidasRestantes > 0) {
-      return this.irAte(novaLinhaAlvo, novaColunaAlvo);
-    } else {
-      return false;
-    }
   }
 
   pegarComida() {
     this.comidasCapturadas++;
-    this.ambiente.comidasRestantes--;
-    const comida = this.ambiente.comidas.find(
-      (comida) => comida.coluna === this.coluna && comida.linha === this.linha
-    );
-    comida.pega = true;
     this.valor = `🐜${"🌿".repeat(this.comidasCapturadas)}`;
     return this.valor;
   }
 
-  irDireita() {
-    this.modificarAmbienteAnterior();
-    this.coluna++;
-    return this.modificarAmbientePosterior();
-  }
-
-  irEsquerda() {
-    this.modificarAmbienteAnterior();
-    this.coluna--;
-    return this.modificarAmbientePosterior();
-  }
-
-  irCima() {
-    this.modificarAmbienteAnterior();
-    this.linha--;
-    return this.modificarAmbientePosterior();
-  }
-
-  irBaixo() {
-    this.modificarAmbienteAnterior();
-    this.linha++;
-    return this.modificarAmbientePosterior();
-  }
-
-  irAte(linhaAlvo, colunaAlvo) {
-    while (this.ambiente.comidasRestantes !== 0) {
-      this.cont++;
-
-      if (this.coluna > colunaAlvo) {
-        this.movimentos.push("esquerda");
-        return this.irEsquerda(this.linha, this.coluna);
-      } else if (this.coluna < colunaAlvo) {
-        this.movimentos.push("direita");
-        return this.irDireita();
-      } else {
-        if (this.linha > linhaAlvo) {
-          this.movimentos.push("cima");
-          return this.irCima();
-        } else {
-          this.movimentos.push("baixo");
-          return this.irBaixo();
+  irAteNinho() {
+    this.ambiente.ambiente.map((linha, indexLinha) => {
+      if (indexLinha % 2 == 0) {
+        for (let i = 0; i < linha.length; i++) {
+          this.ambiente.ambiente[indexLinha][i] = this.ambiente.temComida(
+            indexLinha,
+            i
+          )
+            ? this.pegarComida()
+            : this.valor;
+          if (i != 0) {
+            this.ambiente.ambiente[indexLinha][i - 1] = "V";
+          }
+          this.ambiente.mostrar();
         }
-      }
-    }
-    return;
-  }
-
-  irAteNinho(linhaAlvo, colunaAlvo) {
-    while (this.linha !== linhaAlvo || this.coluna !== colunaAlvo) {
-      if (this.coluna > colunaAlvo) {
-        this.movimentos.push("esquerda");
-        return this.irEsquerda(this.linha, this.coluna);
-      } else if (this.coluna < colunaAlvo) {
-        this.movimentos.push("direita");
-        return this.irDireita();
+        this.ambiente.ambiente[indexLinha][4] = "V";
       } else {
-        console.log("coluna igual");
-        if (this.linha > linhaAlvo) {
-          this.movimentos.push("cima");
-          return this.irCima();
-        } else {
-          this.movimentos.push("baixo");
-          return this.irBaixo();
+        for (let i = 4; i >= 0; i--) {
+          this.ambiente.ambiente[indexLinha][i] = this.ambiente.temComida(
+            indexLinha,
+            i
+          )
+            ? this.pegarComida()
+            : this.valor;
+          if (i != 4) {
+            this.ambiente.ambiente[indexLinha][i + 1] = "V";
+          }
+          this.ambiente.mostrar();
         }
+        this.ambiente.ambiente[indexLinha][0] = "V";
       }
-    }
+    });
     this.chegouNoNinho = true;
-    console.log("chegou aq");
     this.ambiente.mostrar();
     return;
   }
@@ -148,27 +69,33 @@ class Ambiente {
 
   constructor() {
     this.gerar();
+    this.mostrar();
     this.comidas = this.mapearComidas();
     this.comidasRestantes = this.comidas.length;
   }
   gerar() {
     const ambiente = [
-      [[], [], [], [], []],
-      [[], [], [], [], []],
-      [[], [], [], [], []],
-      [[], [], [], [], []],
-      [[], [], [], [], []],
+      ["🐜", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", "N"],
     ];
 
-    let formiga = { valor: "🐜", quantidade: 0, limite: 1 };
     let comida = { valor: "🌿", quantidade: 0, limite: 3, min: 1 };
     let ninho = { valor: "N", quantidade: 0, limite: 1 };
     let predador = { valor: "P", quantidade: 0, limite: 0 };
     let vazio = { valor: "V", quantidade: 0, limite: -1 };
-    let elementos = [comida, ninho, predador, formiga];
+    let elementos = [comida, predador];
 
     ambiente.forEach((linha, indexLinha) => {
       linha.forEach((celula, indexCelula) => {
+        if (
+          (indexLinha === 0 && indexCelula === 0) ||
+          (indexLinha === 4 && indexCelula === 4)
+        ) {
+          return;
+        }
         let elementoAux = elementos.random();
         let celulaValor =
           elementoAux.quantidade !== elementoAux.limite ? elementoAux : vazio;
@@ -183,6 +110,7 @@ class Ambiente {
         ambiente[indexLinha][indexCelula] = celula?.valor;
       });
     });
+
     return (this.ambiente = ambiente);
   }
 
@@ -279,4 +207,4 @@ formiga = new Formiga(
   ambiente
 );
 
-formiga.irAte();
+formiga.irAteNinho();
